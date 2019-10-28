e74049b59dd2b99331177d6593298f2ebe558d21
Raymond Chong <mathsgod@yahoo.com>
2019-10-28 12:41:16 +0800

    init

---
+++ src/DataUrl.php
@@ -0,0 +1,26 @@
+<?
+
+namespace HTTP;
+
+class DataUrl
+{
+
+    public $mime_type = "";
+
+    public function __construct(string $dataurl)
+    {
+        list($this->mime_type, $data) = explode(';', $dataurl);
+        list(, $data) = explode(',', $data);
+        $this->data = base64_decode($data);
+    }
+
+    public function writeTo(string $filename)
+    {
+        return file_put_contents($filename, $this->data);
+    }
+
+    public function getMimeType()
+    {
+        return $this->mime_type;
+    }
+}
